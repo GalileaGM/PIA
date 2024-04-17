7f773a256728b9d8876a19c95b487b33f4255e7b
Marcela Pedraza <pedrazamarcelyamileth@gmail.com>
2024-04-17 15:04:20 -0600

    agregar cambios

---
+++ VentanasPIA/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace VentanasPIA
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            checkBox1 = new CheckBox();
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            label5 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(209, 186);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Nombre:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(276, 183);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(165, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = Color.Black;
+            label2.Location = new Point(209, 222);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Email:";
+            label2.Click += label2_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(276, 212);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(160, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = Color.Black;
+            label3.Location = new Point(203, 253);
+            label3.Name = "label3";
+            label3.Size = new Size(60, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Password:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(276, 245);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(160, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = Color.Black;
+            label4.Location = new Point(146, 286);
+            label4.Name = "label4";
+            label4.Size = new Size(117, 15);
+            label4.TabIndex = 6;
+            label4.Text = "Confirmar Password:";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(276, 283);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(160, 23);
+            textBox4.TabIndex = 7;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.ForeColor = Color.Black;
+            checkBox1.Location = new Point(224, 323);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(128, 19);
+            checkBox1.TabIndex = 8;
+            checkBox1.Text = "mostrar contraseña";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.SlateBlue;
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(261, 364);
+            button1.Name = "button1";
+            button1.Size = new Size(113, 43);
+            button1.TabIndex = 9;
+            button1.Text = "Sing up";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.logoBolegrama;
+            pictureBox1.Location = new Point(180, -1);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(0, 0);
+            pictureBox1.TabIndex = 10;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.logoBolegrama;
+            pictureBox2.Location = new Point(246, 12);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(157, 115);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 11;
+            pictureBox2.TabStop = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.ForeColor = Color.Black;
+            label5.Location = new Point(283, 147);
+            label5.Name = "label5";
+            label5.Size = new Size(69, 15);
+            label5.TabIndex = 12;
+            label5.Text = "REGISTRATE";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightSteelBlue;
+            ClientSize = new Size(682, 450);
+            Controls.Add(label5);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(button1);
+            Controls.Add(checkBox1);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            ForeColor = SystemColors.GradientActiveCaption;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox4;
+        private CheckBox checkBox1;
+        private Button button1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Label label5;
+    }
+}
---
+++ VentanasPIA/Form2.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace VentanasPIA
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            monthCalendar1 = new MonthCalendar();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            trackBar1 = new TrackBar();
+            button3 = new Button();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            button4 = new Button();
+            label15 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.conciertobad;
+            pictureBox1.Location = new Point(2, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(218, 164);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 170);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Eventos";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(126, 170);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "Acerca de";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // monthCalendar1
+            // 
+            monthCalendar1.Location = new Point(315, 31);
+            monthCalendar1.Name = "monthCalendar1";
+            monthCalendar1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(22, 222);
+            label1.Name = "label1";
+            label1.Size = new Size(51, 15);
+            label1.TabIndex = 6;
+            label1.Text = "MEXICO";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(22, 263);
+            label2.Name = "label2";
+            label2.Size = new Size(32, 15);
+            label2.TabIndex = 7;
+            label2.Text = "AGO";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(22, 278);
+            label3.Name = "label3";
+            label3.Size = new Size(19, 15);
+            label3.TabIndex = 8;
+            label3.Text = "30";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(94, 247);
+            label4.Name = "label4";
+            label4.Size = new Size(73, 15);
+            label4.TabIndex = 9;
+            label4.Text = "CERCA DE TI";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(94, 263);
+            label5.Name = "label5";
+            label5.Size = new Size(60, 15);
+            label5.TabIndex = 10;
+            label5.Text = "vie - 21:00";
+            label5.Click += label5_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(75, 278);
+            label6.Name = "label6";
+            label6.Size = new Size(206, 15);
+            label6.TabIndex = 11;
+            label6.Text = "Monterrey, NL Auditorio Citibanamex";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(94, 302);
+            label7.Name = "label7";
+            label7.Size = new Size(73, 15);
+            label7.TabIndex = 12;
+            label7.Text = "BAD BUNNY";
+            // 
+            // trackBar1
+            // 
+            trackBar1.Location = new Point(2, 320);
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(700, 45);
+            trackBar1.TabIndex = 13;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.SlateBlue;
+            button3.Location = new Point(502, 259);
+            button3.Name = "button3";
+            button3.Size = new Size(161, 52);
+            button3.TabIndex = 14;
+            button3.Text = "BUSCAR BOLETOS";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(22, 350);
+            label8.Name = "label8";
+            label8.Size = new Size(51, 15);
+            label8.TabIndex = 15;
+            label8.Text = "MEXICO";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(22, 378);
+            label9.Name = "label9";
+            label9.Size = new Size(32, 15);
+            label9.TabIndex = 16;
+            label9.Text = "AGO";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(22, 393);
+            label10.Name = "label10";
+            label10.Size = new Size(19, 15);
+            label10.TabIndex = 17;
+            label10.Text = "31";
+            label10.Click += label10_Click;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(94, 368);
+            label11.Name = "label11";
+            label11.Size = new Size(73, 15);
+            label11.TabIndex = 18;
+            label11.Text = "CERCA DE TI";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(94, 383);
+            label12.Name = "label12";
+            label12.Size = new Size(60, 15);
+            label12.TabIndex = 19;
+            label12.Text = "vie - 21:00";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(60, 398);
+            label13.Name = "label13";
+            label13.Size = new Size(206, 15);
+            label13.TabIndex = 20;
+            label13.Text = "Monterrey, NL Auditorio Citibanamex";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(94, 426);
+            label14.Name = "label14";
+            label14.Size = new Size(73, 15);
+            label14.TabIndex = 21;
+            label14.Text = "BAD BUNNY";
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.SlateBlue;
+            button4.Location = new Point(502, 359);
+            button4.Name = "button4";
+            button4.Size = new Size(161, 52);
+            button4.TabIndex = 22;
+            button4.Text = "BUSCAR BOLETOS";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(94, 450);
+            label15.Name = "label15";
+            label15.Size = new Size(111, 15);
+            label15.TabIndex = 23;
+            label15.Text = "Poca disponibilidad";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightSteelBlue;
+            ClientSize = new Size(800, 483);
+            Controls.Add(label15);
+            Controls.Add(button4);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(button3);
+            Controls.Add(trackBar1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(monthCalendar1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Name = "Form2";
+            Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button2;
+        private MonthCalendar monthCalendar1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TrackBar trackBar1;
+        private Button button3;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Button button4;
+        private Label label15;
+    }
+}
